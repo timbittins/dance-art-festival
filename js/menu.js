30475c7d8efa740031afebb9d172c0d3762946a2
Tim Bittins <tech@timbittins.com>
2021-09-19 17:44:46 +0200

    each html file is autak

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,29 @@
-"use-strict";
-console.log("JS Start check");
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-const menuListItems = [...document.getElementsByClassName("menu__link")];
-const removeClassFromMenuList = () =>
-  menuListItems.forEach((menuListItem) => {
-    menuListItem.classList.remove("current");
-  });
+"use strict";
+const log = console.log.bind(console);
+function getSelector(getElement) {
+  const element = document.querySelector(getElement);
+  if (element) {
+    return element;
+  } else {
+    throw Error(`There is no element called ${getElement}`);
+  }
+}
+function getSelectors(getElements) {
+  const elements = document.querySelectorAll(getElements);
+  if (elements.length < 1) {
+    throw Error(`There are no elements called ${getElements}`);
+  } else {
+    return elements;
+  }
+}
+function getID(getElementID) {
+  const elementID = document.getElementById(getElementID);
+  if (elementID) {
+    return elementID;
+  } else {
+    throw Error(`There is no ID called ${getElementID}`);
+  }
+}
 
 // M E N U
 // Landing Page Load
