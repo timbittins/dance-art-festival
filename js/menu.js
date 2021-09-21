a9ad5ac2e0e91088c8e92701e5fb723d8b64d6d1
Tim Bittins <tech@timbittins.com>
2021-09-21 10:18:48 +0200

    implemented grid | menu js finished

--- js/menu.js
+++ js/menu.js
@@ -3,22 +3,31 @@ import { log, getSelector, getSelectors, getID } from "./setup.js";
 autoMenuSwitch();
 function autoMenuSwitch() {
   const navCheckbox = getID("navigation");
-  const menuBars = getSelectors('[data-menu]');
+  const menuBars = [...getSelectors("[data-menu]")];
+  const menuButton = getSelector(".menu__button");
 
+  // On load
   if (window.innerWidth > 1050) {
+    log("bigger then 1050");
     navCheckbox.checked = true;
-    menuBars.forEach((menuBar) => (menuBar.style.transition = "none"));
+    menuBars.forEach((menuBar) => (menuBar.style.transitionDuration = "0s"));
   } else {
     navCheckbox.checked = false;
+    menuBars.forEach((menuBar) => (menuBar.style.transitionDuration = "600ms"));
   }
 
+  // On resize
   window.addEventListener("resize", function () {
-    if (window.innerWidth > 1050) {
-      navCheckbox.checked = true;
-    } else {
-      navCheckbox.checked = false;
-    }
+    log("resize");
+    if (window.innerWidth < 1050) navCheckbox.checked = false;
+  });
+
+  // On menubutton click
+  menuButton.addEventListener("click", function () {
+    log("button clicked");
+    menuBars.forEach((menuBar) => (menuBar.style.transitionDuration = "600ms"));
   });
 }
 
 export default autoMenuSwitch;
+
